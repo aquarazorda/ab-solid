4a5b05e0ce3e5e8f04a1feba9787622c3e0bf513
givigelashvili <55489231+aquarazorda@users.noreply.github.com>
2023-05-07 21:15:44 +0400

    Lazy loading widget images

--- src/components/mobile/Sliders/WidgetSlider.tsx
+++ src/components/mobile/Sliders/WidgetSlider.tsx
@@ -82,7 +82,7 @@ export const WidgetSlider = (props: Props) => {
                 <div class="_s_size-w-percent--25 _s_flex _s_overflow-hidden _s_pl-1 _s_pr-1 _s_lg-pl-none _s_lg-pr-none">
                   <Show when={loaded[idx()]}>
                     <img
-                      loading={idx() > 1 ? "lazy" : "eager"}
+                      loading="lazy"
                       class="_s_size-w-percent--25 _s_flex _s_b-radius-sm _s_lg-b-radius-none"
                       src={createStaticUrl(`/images/common/${game.id}_${locale()}.jpg`)}
                       alt={t(game.title.langId)}
